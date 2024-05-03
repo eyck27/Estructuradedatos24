51fa20fe655702e1511abbee9ecf8056cc10ccf1
eyck27 <167552304+eyck27@users.noreply.github.com>
2024-05-02 22:30:48 -0500

    ejercici 1

--- lab 2/lab 2/Program.cs
+++ lab 2/lab 2/Program.cs
@@ -1,11 +1,32 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hola clase estructura de datos!");
-// sumar  lo n primeros numeros
-var n = 2;
-var suma = 0;
-for (var i = 1; i <= n; i++)
+﻿using System;
+
+class Program
 {
-    suma += i;
+    static void Main()
+    {
+        // Solicitar al usuario que ingrese dos números
+        Console.WriteLine("Ingrese el primer número:");
+        float num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo número:");
+        float num2 = float.Parse(Console.ReadLine());
+
+        // Realizar operaciones
+        float suma = num1 + num2;
+        float resta = num1 - num2;
+        float multiplicacion = num1 * num2;
+
+        // Verificar si el segundo número es diferente de cero antes de realizar la división
+        float division;
+        if (num2 != 0)
+            division = num1 / num2;
+        else
+            division = 0; // División por cero, asignar 0
+
+        // Mostrar resultados
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Resta: " + resta);
+        Console.WriteLine("Multiplicación: " + multiplicacion);
+        Console.WriteLine("División: " + division);
+    }
 }
-Console.WriteLine($"la suma de los {n} numeros naturales son :{suma}");
+
