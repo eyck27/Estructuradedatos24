e567e4e5c9f01c06d12d81d50c06e5b832ce8fde
eyck27 <167552304+eyck27@users.noreply.github.com>
2024-05-02 21:34:20 -0500

    codigo lab 1

---
+++ lab 2/lab 2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("hola clase estructura de datos!");
+// sumar  lo n primeros numeros
+var n = 2;
+var suma = 0;
+for (var i = 1; i <= n; i++)
+{
+    suma += i;
+}
+Console.WriteLine($"la suma de los {n} numeros naturales son :{suma}");
